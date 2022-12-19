97482710fb7e4c53c0004b786b4f003ba4a70910
ogvieira <osmargv100@gmail.com>
2022-12-19 14:41:24 -0300

    primeiro commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace maui_game_gibbet;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_game_gibbet;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
